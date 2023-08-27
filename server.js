ce089b71b072aae0480a5c42310ab25bdd78a427
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-08-27 15:21:35 +0300

    Recreate branch hw-03

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const { DB_URI } = process.env;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log("CONNECT SUCCESS");
+  })
+
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
