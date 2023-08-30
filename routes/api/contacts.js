26dc7b296647e95cf0fe8184846e66cc1657a56d
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-08-31 01:22:08 +0300

    Connect middleware isValidId for routes with Id. Create router.patch

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,16 +2,30 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
+const {
+  isValidId,
+  // validateBody
+} = require("../../middlewares");
+
+// const { updateFavoriteSchema } = require("../../schemas/contacts");
+
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
 router.post("/", ctrl.add);
 
-router.put("/:contactId", ctrl.updateById);
+router.put("/:contactId", isValidId, ctrl.updateById);
 
-router.delete("/:contactId", ctrl.deleteById);
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  // validateBody(updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
+
+router.delete("/:contactId", isValidId, ctrl.deleteById);
 
 module.exports = router;
