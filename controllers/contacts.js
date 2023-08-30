805faa09cc6881851b2d21427c547cd450e98079
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-08-31 01:20:00 +0300

    Change code in controllers for REST API. Add method updateStatusContact

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,17 +1,17 @@
-const contacts = require("../models/contacts.js");
-
 const schema = require("../schemas/contacts.js");
+
+const Contact = require("../models/contact.js");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const allContacts = await contacts.listContacts();
+  const allContacts = await Contact.find();
   res.json(allContacts);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const oneContact = await contacts.getContactById(contactId);
+  const oneContact = await Contact.findById(contactId);
   if (!oneContact) {
     throw HttpError(404, "Contact not found");
   }
@@ -27,46 +27,39 @@ const add = async (req, res) => {
     throw HttpError(400, error.message);
   }
 
-  const newContact = await contacts.addContact(req.body);
+  const newContact = await Contact.create(req.body);
   res.status(201).json(newContact);
 };
 
 const updateById = async (req, res) => {
-  const { error } = schema.contactSchemaNoRequired.validate(req.body);
-
-  if (error) {
-    throw HttpError(400, error.message);
-  }
-
   const { contactId } = req.params;
   const body = req.body;
 
-  // if (Object.keys(body).length === 0) {
-  //   res.statusCode = 400;
-  //   res.json({ message: "missing fields" });
-  //   return;
-  // }
-
-  const updateContact = await contacts.updateContactById(
-    contactId,
-    body
-    // , {
-    // new: true,
-    // }
-  );
-
+  const updateContact = await Contact.findByIdAndUpdate(contactId, body, {
+    new: true,
+  });
   if (!updateContact) {
     throw HttpError(400, "Not found");
   }
+  res.json(updateContact);
+};
 
-  // res.statusCode = 200;
-  // res.json({ updateContact });
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const body = req.body;
+
+  const updateContact = await Contact.findByIdAndUpdate(contactId, body, {
+    new: true,
+  });
+  if (!updateContact) {
+    throw HttpError(400, "Not found");
+  }
   res.json(updateContact);
 };
 
 const deleteById = async (req, res) => {
   const { contactId } = req.params;
-  const removeContact = await contacts.removeContactById(contactId);
+  const removeContact = await Contact.findByIdAndRemove(contactId);
 
   if (!removeContact) {
     throw HttpError(400, "Not found");
@@ -80,5 +73,6 @@ module.exports = {
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
   updateById: ctrlWrapper(updateById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
   deleteById: ctrlWrapper(deleteById),
 };
