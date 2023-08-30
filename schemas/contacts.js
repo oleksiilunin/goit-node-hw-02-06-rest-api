6e74860b1b7b237aef1167e9e7b3fa17db5f7f4c
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-08-31 01:07:55 +0300

    Create updateFavoriteSchema. Move patterns to variables

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,33 +1,32 @@
 const Joi = require("joi");
 
+const nameRegexp =
+  /^[a-zA-Zа-яА-Я0-9]+(([' -][a-zA-Zа-яА-Я0-9 ])?[a-zA-Zа-яА-Я0-9]*)*$/;
+const phoneRegexp =
+  /\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}/;
+
+// TODO: email and phone not require ? (in model no require)
+
 const contactSchemaIsRequired = Joi.object({
-  name: Joi.string()
-    .max(24)
-    .pattern(
-      /^[a-zA-Zа-яА-Я0-9]+(([' -][a-zA-Zа-яА-Я0-9 ])?[a-zA-Zа-яА-Я0-9]*)*$/
-    )
-    .required(),
+  name: Joi.string().max(24).pattern(nameRegexp).required(),
   email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(
-      /\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}/
-    )
-    .required(),
+  phone: Joi.string().pattern(phoneRegexp).required(),
+  favorite: Joi.boolean().default(false),
 });
 
 const contactSchemaNoRequired = Joi.object({
-  name: Joi.string()
-    .max(24)
-    .pattern(
-      /^[a-zA-Zа-яА-Я0-9]+(([' -][a-zA-Zа-яА-Я0-9 ])?[a-zA-Zа-яА-Я0-9]*)*$/
-    ),
+  name: Joi.string().max(24).pattern(nameRegexp),
   email: Joi.string().email(),
-  phone: Joi.string().pattern(
-    /\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}/
-  ),
+  phone: Joi.string().pattern(phoneRegexp),
+  favorite: Joi.boolean().default(false),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 module.exports = {
   contactSchemaIsRequired,
   contactSchemaNoRequired,
+  updateFavoriteSchema,
 };
