eaaed03884b818dcd94a0e3faff250b9ec5c4b07
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-09-11 04:06:59 +0300

    Rename schema for addContact and add require  messages

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -5,28 +5,30 @@ const nameRegexp =
 const phoneRegexp =
   /\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}/;
 
-// TODO: email and phone not require ? (in model no require)
-
-const contactSchemaIsRequired = Joi.object({
-  name: Joi.string().max(24).pattern(nameRegexp).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().pattern(phoneRegexp).required(),
-  favorite: Joi.boolean().default(false),
-});
-
-const contactSchemaNoRequired = Joi.object({
-  name: Joi.string().max(24).pattern(nameRegexp),
-  email: Joi.string().email(),
-  phone: Joi.string().pattern(phoneRegexp),
+const addContactSchema = Joi.object({
+  name: Joi.string().max(24).pattern(nameRegexp).required().messages({
+    "any.required": "Field 'name' is missing",
+    "string.pattern.base":
+      "Name may contain only letters, apostrophe, dash, and spaces. For example, Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
+  }),
+  email: Joi.string().email().required().messages({
+    "any.required": "Field 'email' is missing",
+  }),
+  phone: Joi.string().pattern(phoneRegexp).required().messages({
+    "any.required": "Field 'phone' is missing",
+    "string.pattern.base":
+      "Phone number must be a valid phone number, digits and can contain spaces, dashes, parentheses and can start with +",
+  }),
   favorite: Joi.boolean().default(false),
 });
 
 const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  favorite: Joi.boolean()
+    .required()
+    .messages({ "any.required": "Missing field favorite" }),
 });
 
 module.exports = {
-  contactSchemaIsRequired,
-  contactSchemaNoRequired,
+  addContactSchema,
   updateFavoriteSchema,
 };
