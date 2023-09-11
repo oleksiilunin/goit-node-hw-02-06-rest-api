97306abc88b3b3efdc5feeb619799b30a441af51
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-09-11 04:18:10 +0300

    Add middleware validateBody

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,22 +2,18 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-// const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 
-// const { registerSchema } = require("../../schemas/user");
+const { registerSchema, loginSchema } = require("../../schemas/user");
 
 const router = express.Router();
 
-router.post(
-  "/register",
-  // validateBody(registerSchema),
-  ctrl.register
-);
+router.post("/register", validateBody(registerSchema), ctrl.register);
 
-router.post(
-  "/login",
-  // validateBody(loginSchema),
-  ctrl.login
-);
+router.post("/login", validateBody(loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
