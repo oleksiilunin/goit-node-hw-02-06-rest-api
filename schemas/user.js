c137d881a568f3a9a0d20acf4d87b6362d03f96e
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-09-19 20:31:25 +0300

    Add verufyEmailSchema

--- schemas/user.js
+++ schemas/user.js
@@ -11,6 +11,13 @@ const registerSchema = Joi.object({
   password: Joi.string().min(8).required(),
 });
 
+const verifyEmailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required().messages({
+    "string.email": "Email should be a valid email address",
+    "any.required": "Email is required",
+  }),
+});
+
 const loginSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(8).required(),
@@ -18,5 +25,6 @@ const loginSchema = Joi.object({
 
 module.exports = {
   registerSchema,
+  verifyEmailSchema,
   loginSchema,
 };
